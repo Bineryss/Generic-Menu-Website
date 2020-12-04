3a0f9ae42d7eaca016d4b7bde777f3395806feee
sebastian <binerys@outlook.de>
2020-12-05 00:33:23 +0100

    finding a design

--- src/component/navbar/index.tsx
+++ src/component/navbar/index.tsx
@@ -1,14 +1,14 @@
+import { AppBar, Toolbar } from "@material-ui/core"
 import React from "react"
-import { Navbar } from "react-bootstrap"
+
 
 const NavBar: React.FC = () => {
     return (
-        <Navbar bg="primary" variant="dark" sticky="top">
-            <Navbar.Brand href="#home" >Speisekarten Website</ Navbar.Brand>
-            <Navbar.Collapse className="justify-content-end">
-                <Navbar.Text >45€</Navbar.Text>
-            </Navbar.Collapse>
-        </Navbar>
+        <AppBar position={"sticky"}>
+            <Toolbar>
+                
+            </Toolbar>
+        </AppBar>
     )
 }
 
